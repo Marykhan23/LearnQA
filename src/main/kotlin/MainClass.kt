eb4081978d9aeb505fd31d800f496d85849841c3
Maria <mariia.khannanova@sbs.com.au>
2023-07-23 21:51:13 +1000

    Test 2 and Test 3

--- src/main/kotlin/MainClass.kt
+++ src/main/kotlin/MainClass.kt
@@ -1,2 +1,18 @@
 class MainClass {
+    private val classNumber = 20
+    private val classString = "hello, world"
+
+    public fun getLocalNumber(): Int {
+        return 14
+    }
+
+    public fun getClassNumber(): Int{
+        println("classNumber $classNumber")
+        return classNumber
+    }
+
+    public fun getClassString(): String{
+        println("classNumber $classNumber")
+        return classString
+    }
 }
--- src/test/kotlin/MainClassTest.kt
+++ src/test/kotlin/MainClassTest.kt
@@ -1,2 +1,23 @@
+
+import org.junit.Assert
+import org.junit.Test
+
+
 class MainClassTest {
+    @Test
+    fun testGetLocalNumber(){
+        Assert.assertTrue("Returns not number 14!", MainClass().getLocalNumber() == 14)
+    }
+
+    @Test
+    fun testGetClassNumber(){
+        Assert.assertTrue("Returns more than number 45!", MainClass().getClassNumber() <= 45)
+    }
+
+    @Test
+    fun testGetClassString(){
+        Assert.assertTrue("I coulnd't find \"hello\" in the string", MainClass().getClassString().uppercase().contains("hello".uppercase()))
+    }
+
+
 }
