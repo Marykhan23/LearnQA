14cccd79df2bace0e42fd52746dfb6249410e404
Maria <mariia.khannanova@sbs.com.au>
2023-08-20 22:16:41 +1000

    Lesson 2

--- src/test/kotlin/FirstTest.kt
+++ src/test/kotlin/FirstTest.kt
@@ -3,13 +3,41 @@ import io.appium.java_client.AppiumDriver
 import io.appium.java_client.MobileElement
 import io.appium.java_client.android.AndroidDriver
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import org.openqa.selenium.By
+import org.openqa.selenium.WebElement
 import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
 import java.net.URL
 
 class FirstTest {
     private var driver: AppiumDriver<MobileElement>? = null
+
+    fun waitForElement(by: By): WebElement{
+        val wait = WebDriverWait(driver, 5)
+        wait.withMessage("Can't find the element: $by")
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by))
+    }
+
+    fun waitForElementTimeout(by: By, timeInSec: Long ): WebElement{
+        val wait = WebDriverWait(driver, timeInSec)
+        wait.withMessage("Can't find the element: $by")
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by))
+    }
+
+    fun assertElementHasText(by: By, text: String){
+        val textElement = waitForElement(by).getAttribute("text")
+        Assert.assertEquals("The text doesn't match the $by  element text", text, textElement)
+    }
+
+    fun waitForElementNotPresent(by: By): Boolean{
+        val wait = WebDriverWait(driver, 5)
+        wait.withMessage("The element: $by is visible")
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(by))
+    }
 
     @Before
     @Throws(Exception::class)
@@ -34,8 +62,30 @@ class FirstTest {
     }
 
     @Test
-    fun firstTest(){
-        println("first test")
+    fun searchForWordThenClearResults(){
+        waitForElement(By.xpath("//*[contains(@text,'Search Wikipedia')]"))?.click()
+        waitForElement(By.id("search_src_text"))?.sendKeys("Java")
+        assertElementHasText(By.id("search_src_text"), "Java")
+        assertElementHasText(By.xpath("//*[contains(@text,'Java')]"), "Java")
+        assertElementHasText(By.xpath("//*[contains(@text,'JavaScript')]"), "JavaScript")
+        waitForElement(By.id("search_close_btn"))?.click()
+        assertElementHasText(By.id("search_src_text"), "Search…")
+        waitForElementNotPresent(By.xpath("//*[contains(@text,'JavaScript')]"))
+    }
+
+    @Test
+    fun searchAndCheckList(){
+        waitForElement(By.xpath("//*[contains(@text,'Search Wikipedia')]"))?.click()
+        waitForElement(By.id("search_src_text"))?.sendKeys("Java")
+        assertElementHasText(By.id("search_src_text"), "Java")
+
+        val elements : MutableList<MobileElement>? = driver?.findElements(By.id("page_list_item_title"))
+        for (element in elements!!){
+            val text = element.getAttribute("text")
+            println(text)
+            Assert.assertTrue("It doesn't include the text $text", text.lowercase().contains("java"))
+        }
+//        elements.forEach { el -> el.getAttribute("text").lowercase().contains("java") }
     }
 
 }
